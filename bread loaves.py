3c2e0a89d49b05e9f2c2cd85d75bab038266c4bd
sai <112242683+saikiranchandu@users.noreply.github.com>
2022-09-26 11:22:00 +0530

    Add files via upload

---
+++ bread loaves.py
@@ -0,0 +1,6 @@
+x=int(input("enter the number of bread loaves of a day old being purchsed:"))
+y=x*185
+print("loaves discount")
+print("regular price",y)
+print("total discount",y*60/100)
+print("total amount to be paid",y-(y*60/100))
