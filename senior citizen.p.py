94eb377d0889a9f2040136a9c9b3c2ad9b06fdc2
sai <112242683+saikiranchandu@users.noreply.github.com>
2022-09-29 13:08:36 +0530

    Add files via upload

---
+++ senior citizen.p.py
@@ -0,0 +1,15 @@
+print("Enter the principal amount:")
+p=int(input())
+
+print("Enter the number of years:")
+t=float(input())
+print("Is the customer is senior citizen :(Y/N)")
+ch=(input())
+if(ch=="y" or ch=="Y"):
+    r=12
+else:
+    r=10
+    
+SI=(p*t*r)/100
+print("simple intrest:",SI)
+
